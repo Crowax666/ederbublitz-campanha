7c1b05f4fbc94c3dc0596b034e424bbb768bf727
Crowax666 <78364279+Crowax666@users.noreply.github.com>
2026-09-08 00:22:42 -0300

    Load editorial polish for home proposal cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./perf.css";
 import "./hero-polish.css";
+import "./cards-polish.css";
 import { pageMetadata } from "../db/seo";
 import MetaPixel from "./MetaPixel";
 import AccessTracker from "./AccessTracker";
